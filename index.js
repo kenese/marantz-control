17a2dc7cf25f1c8d833584e53bec0b2f94e1110e
Kenese Lautusi <kenese.lautusi@xero.com>
2022-01-20 15:20:30 +1300

    working basic, no server yet

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
 // let AVReceiver = require('marantz-avr');
 
 const {AVReceiver, SOURCES, SURROUND_MODES, ZONES} = require("./AVReceiver");
+const {url} = require('./receiver-url.json')
 
 async function main() {
 
-    const receiver = new AVReceiver('101.100.138.116:8088', ZONES.Main);
-
-    // Turns on/off stereo
-    // console.log(await receiver.setPowerState(true));
-    // await receiver.setVolumeLevel(-30)
-    // console.log(await receiver.getSurroundMode());
-    // console.log(await receiver.setInputSource(SOURCES.DVD));
-    // console.log(await receiver.setSurroundMode(SURROUND_MODES.PURE_DIRECT));
-    // console.log(await receiver.getSurroundMode());
-
+    const receiver = new AVReceiver(url, ZONES.Main);
 
     // const result = await setInputToVinyl(receiver);
     const result = await setInputToCast(receiver);
@@ -27,22 +19,17 @@ async function updateReceiver(receiver, source, sound) {
     // await receiver.setVolumeLevel(-30)
 
     const sourceUpdateResult = await receiver.getInputSource();
-
-    let surroundUpdateResult = await receiver.getSurroundMode();
-    // console.log('surroundUpdateResult', surroundUpdateResult);
-    // await receiver.setSurroundMode(sound);
-    // surroundUpdateResult = await receiver.getSurroundMode();
-    // console.log('surroundUpdateResult', surroundUpdateResult);
+    const surroundUpdateResult = await receiver.getSurroundMode();
 
     return `Source set to ${sourceUpdateResult}, sound set to ${surroundUpdateResult}`;
 }
 
 const setInputToVinyl = async (receiver) => {
-    return updateReceiver(receiver, SOURCES.DVD, SURROUND_MODES.PURE_DIRECT);
+    return await updateReceiver(receiver, SOURCES.DVD, SURROUND_MODES.PURE_DIRECT);
 }
 
 const setInputToCast = async (receiver) => {
-    return updateReceiver(receiver, SOURCES.MEDIA_PLAYER, SURROUND_MODES.STEREO);
+    return await updateReceiver(receiver, SOURCES.MEDIA_PLAYER, SURROUND_MODES.STEREO);
 }
 
 main();
