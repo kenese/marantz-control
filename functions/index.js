6a2cf9727a30c4cf270bfecc14c3aed55cd39b1b
Kenese Lautusi <kenese.lautusi@xero.com>
2022-01-22 23:13:11 +1300

    power off added

--- functions/index.js
+++ functions/index.js
@@ -3,9 +3,10 @@ const functions = require("firebase-functions");
 const {AVReceiver, ZONES, SOURCES, SURROUND_MODES} = require("./AVReceiver");
 const {url} = require("./receiver-url.json");
 
-const UPDATE_RECEIVER = {
+const UPDATE_RECEIVER_VALUES = {
   vinyl: {source: SOURCES.DVD, sound: SURROUND_MODES.PURE_DIRECT},
   cast: {source: SOURCES.MEDIA_PLAYER, sound: SURROUND_MODES.STEREO},
+  off: {power: "off"},
 };
 
 // Create and Deploy Your First Cloud Functions
@@ -18,20 +19,20 @@ exports.marantzUpdate = functions.https.onRequest(
     async (request, response) => {
       const {value, receiverName} = request.query;
 
-      const receiverValue = UPDATE_RECEIVER[value] ?
-          UPDATE_RECEIVER[value] :
-          UPDATE_RECEIVER.cast;
+      const receiverValueToUpdate = UPDATE_RECEIVER_VALUES[value] ?
+          UPDATE_RECEIVER_VALUES[value] :
+          UPDATE_RECEIVER_VALUES.cast;
 
       const zone = ZONES[receiverName] ?
           ZONES[receiverName] :
           ZONES.Main;
 
       const receiver = new AVReceiver(url, zone);
-      let result = await updateReceiver(receiver, receiverValue);
+      let result = await updateReceiver(receiver, receiverValueToUpdate);
 
       if (receiverName === "both") {
         receiver.zone = ZONES.Zone2;
-        result = `\n${await updateReceiver(receiver, receiverValue)}`;
+        result = `\n${await updateReceiver(receiver, receiverValueToUpdate)}`;
       }
 
       functions.logger.info(result, {structuredData: true});
@@ -39,6 +40,11 @@ exports.marantzUpdate = functions.https.onRequest(
     });
 
 async function updateReceiver(receiver, updateValues) {
+  const receiverName = `${receiver.zone ? receiver.zone : "Main"} `;
+  if (updateValues.power) {
+    await receiver.setPowerState(false);
+    return receiverName + "turned off";
+  }
   await receiver.setPowerState(true);
 
   await receiver.setInputSource(updateValues.source);
@@ -48,7 +54,8 @@ async function updateReceiver(receiver, updateValues) {
   const sourceUpdateResult = await receiver.getInputSource();
   const surroundUpdateResult = await receiver.getSurroundMode();
 
-  return `${receiver.zone ? receiver.zone : "Main"} ` +
+
+  return receiverName +
       `source set to ${sourceUpdateResult}, ` +
       `sound set to ${surroundUpdateResult}`;
 }
